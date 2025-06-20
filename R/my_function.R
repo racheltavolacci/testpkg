177ffa331be942b4d8d0135ff3496084fe9752d2
racheltavolacci <rachel@washingtonstem.org>
2025-06-20 14:42:10 -0700

    test test

---
+++ R/my_function.R
@@ -0,0 +1,10 @@
+#' Say Hello
+#'
+#' Prints a greeting.
+#' @export
+hello <- function() {
+  print("Hello, intermediate workshop participants!")
+}
+usethis::use_github() # publishes to your GitHub account
+
+export(hello)
